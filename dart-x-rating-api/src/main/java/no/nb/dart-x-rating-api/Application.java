a4ea45d721c1008f8d47bc72a7ad46fb7fdf4df3
Andreas Bjørnådal <bjornadal@gmail.com>
2015-04-10 09:47:22 +0200

    Added dart-x-rating-api

---
+++ dart-x-rating-api/src/main/java/no/nb/dart-x-rating-api/Application.java
@@ -0,0 +1,12 @@
+package dart-x-rating-api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DartXRatingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DartXRatingApiApplication.class, args);
+    }
+}
