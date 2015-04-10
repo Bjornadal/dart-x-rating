46d7afa9714941c64c25dc8df844ed4ab95b29eb
busemann <r_karstensen@hotmail.com>
2015-04-10 16:09:18 +0200

    Fun Fact

--- dart-x-rating-client/app/scripts/controllers/statisticsController.js
+++ dart-x-rating-client/app/scripts/controllers/statisticsController.js
@@ -1,10 +1,17 @@
 'use strict';
 
-angular.module('dartXRatingApp').controller('StatisticsCtrl', function ($scope, StatisticsService, PlayerFactory, MatchFactory) {
+angular.module('dartXRatingApp').controller('StatisticsCtrl', function ($scope, StatisticsService, PlayerFactory, MatchFactory, AchievementFactory, $interval, $timeout) {
     $scope.players = PlayerFactory();
     $scope.matches = MatchFactory();
+    $scope.achievements = AchievementFactory();
     $scope.startDate = new Date(new Date().setDate(new Date().getDate()-7));
     $scope.endDate = new Date();
+    var funfact = ["Her kommer det masse random funfacts etter hvert...", "... noen må bare laga all den rare statistikken først..."];
+
+    updateFunFact(true);
+    $interval(function() {
+        updateFunFact(false);
+    }, 10000);
 
     StatisticsService.generateStatistics().then(function(players) {
         $scope.players = players;
@@ -28,4 +35,19 @@ angular.module('dartXRatingApp').controller('StatisticsCtrl', function ($scope, 
             $scope.chartData = data;
         });
     });
+
+    function updateFunFact(firstRun) {
+        var delay = 2000;
+        var index = Math.floor(Math.random() * funfact.length);
+        if (firstRun) {
+            delay = 0;
+            index = 0;
+        } else {
+            $scope.showFunFact = false;
+        }
+        $timeout(function() {
+            $scope.funFact = funfact[index];
+            $scope.showFunFact = true;
+        }, delay);
+    }
 });
