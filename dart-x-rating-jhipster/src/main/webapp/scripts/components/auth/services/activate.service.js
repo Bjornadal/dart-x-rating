26bf302a9d6ca18cf7286aeaf47d4d6226428d61
Andreas Bjørnådal <bjornadal@gmail.com>
2015-04-28 07:31:39 +0200

    Init commit

---
+++ dart-x-rating-jhipster/src/main/webapp/scripts/components/auth/services/activate.service.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('dartxratingApp')
+    .factory('Activate', function ($resource) {
+        return $resource('api/activate', {}, {
+            'get': { method: 'GET', params: {}, isArray: false}
+        });
+    });
+
+
