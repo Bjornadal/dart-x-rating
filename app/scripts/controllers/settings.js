aabf278a975a3996a97782211b6acf147ab61eea
Andreas Bjørnådal <bjornadal@gmail.com>
2016-01-08 15:21:28 +0100

    Gametype dropdown

--- app/scripts/controllers/settings.js
+++ app/scripts/controllers/settings.js
@@ -2,6 +2,12 @@
 
 angular.module('xgames').controller('SettingsCtrl', function ($scope, $localstorage, $location, $window) {
     $scope.settings = $localstorage.getObject('settings')
+    $scope.gameTypes = ["Dart", "Basket", "Bordtennis"];
+
+    $scope.setGameType = function(gameType) {
+      $scope.settings.game = gameType;
+    };
+
     $scope.setLeague = function() {
         $localstorage.setObject("settings", $scope.settings);
 
