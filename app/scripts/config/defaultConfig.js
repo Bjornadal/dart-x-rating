628760ccb4f244b257378abfded53aea27b0569f
busemann <r_karstensen@hotmail.com>
2016-02-27 14:11:47 +0100

    Correct module name

--- app/scripts/config/defaultConfig.js
+++ app/scripts/config/defaultConfig.js
@@ -2,7 +2,7 @@
  * Created by andreasb on 18.03.15.
  */
 
-angular.module('dartXRatingApp')
+angular.module('xgames')
     .constant('defaultConfig', {
         'firebaseBackend': 'https://YOUR-FIREBASE.firebaseio.com',
         'production': false
